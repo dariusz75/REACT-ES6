59f0bc4c1d4d11e7bd32b982b807b8cc300c9a14
Dariusz Franczak <darius32@op.pl>
2017-10-26 00:55:24 +0100

    02 work in progress

--- 02-components-state-and-stateless/src/App.js
+++ 02-components-state-and-stateless/src/App.js
@@ -4,35 +4,82 @@ import './App.css';
 
 class App extends Component {
   render() {
-
-      var i = 1;
-      var statement = (function myFunction() {
-        if (i == 2) {
-          return 'True';
-        } else {
-          return 'False';
-        }
-      }());
-      var myStyle = {
-        fontSize: 50,
-        color: 'white',
-        backgroundColor: 'red'
-      };
-
-
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>{statement}</h1>
-        <h1>{2+3}</h1>
-        <p style={myStyle}>Hello World!</p>
+        <Header />
+        <Content />
       </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <p className="App-p">This is Header Component</p>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  constructor (props) {
+    super(props);
+    
+    this.state = {
+      data: [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": "30"
+        },
+        {
+          "id": 2,
+          "name": "John",
+          "age": "35"
+        },
+        {
+          "id": 3,
+          "name": "Frank",
+          "age": "40"
+        },
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          This is Content Component
+        </h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Age</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key={i} data="person"/> )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
     );
   }
 }
