a27251cadb731d912cd775bee23e8a25d86b297e
Dariusz Franczak <darius32@op.pl>
2017-10-25 23:20:03 +0100

    corrections

--- basics/my-app/src/App.js
+++ basics/my-app/src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
 
       var i = 1;
-
       var statement = (function myFunction() {
         if (i == 2) {
           return 'True';
@@ -14,6 +13,12 @@ class App extends Component {
           return 'False';
         }
       }());
+      var myStyle = {
+        fontSize: 50,
+        color: 'white',
+        backgroundColor: 'red'
+      };
+
 
     return (
       <div className="App">
@@ -25,6 +30,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>{statement}</h1>
+        <h1>{2+3}</h1>
+        <p style={myStyle}>Hello World!</p>
       </div>
     );
   }
